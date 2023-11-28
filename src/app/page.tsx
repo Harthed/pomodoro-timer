f1d0c0bda8c4c6faa7a9df2c9cf13b5499e84024
Harthed <keven.figueiral@gmail.com>
2023-11-28 15:01:57 -0300

    logo reduction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
 
 import Image from 'next/image'
 import logo from '@/img/pomologo.png'
-
 import start from '@/img/play.png'
 
 export default function Home() {
   return (
     <main className="bg-zinc-800 h-screen flex flex-col items-center justify-center gap-5">
-  <Image className='lg:self-start ml-8' src={logo} alt="logo"/>
+  <Image className='lg:self-start ml-8 lg:w-40' src={logo} alt="logo"/>
   <div className='flex items-center flex-col gap-4 mt-24'>
   
-  <div className='group w-80 h-80 border-green-500 border-4 rounded-full flex items-center justify-center hover:border-yellow-400 '>
-    <h1 className='font-bold text-white text-5xl group-hover:text-green-400'>12:00</h1>
+  <div className='group w-80 h-80 border-green-500 border-4 rounded-full flex items-center justify-center'>
+    <h1 className='font-bold text-white text-5xl'>12:00</h1>
   </div>
   <Image className='' width={80} src={start} alt='start'/>
   </div>
